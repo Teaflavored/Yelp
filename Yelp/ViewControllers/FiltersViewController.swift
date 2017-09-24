7f4c183624561fe1353e3a55fc88232eec29d9dc
Auster Chen <auster.s.chen@gmail.com>
2017-09-23 23:06:22 -0700

    search stuff

--- Yelp/ViewControllers/FiltersViewController.swift
+++ Yelp/ViewControllers/FiltersViewController.swift
@@ -33,7 +33,33 @@ UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection 
         section: Int) -> Int {
-        return 10
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return SearchSettings.instance.searchRadiusesMap.count
+        case 2:
+            return SearchSettings.instance.sortValuesMap.count
+        case 3:
+            return SearchSettings.instance.categories.count
+        default:
+            return 0
+        }
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return nil
+        case 1:
+            return "Distance"
+        case 2:
+            return "Sort"
+        case 3:
+            return "Category"
+        default:
+            return nil
+        }
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
